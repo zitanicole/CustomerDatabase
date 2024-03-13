b0066eb4fa95370913e9232e5f9ece655e2e6972
zitanicole <zita.kellogg@student.centralia.edu>
2024-03-13 00:04:03 -0700

    front end/web page shows list of hardcoded interpolated customer attribute

--- customerdatabase.client/src/app/app.component.ts
+++ customerdatabase.client/src/app/app.component.ts
@@ -1,7 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+export interface Customer {
+  customerID: number;
+  first: string;
+  last: string;
+  address: string;
+  zip: string;
+  email: string;
+  phone: string;
+  history: string; 
 
+}
 
 @Component({
   selector: 'app-root',
@@ -9,7 +19,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  CustomerList: Customer[] = [
+    {
+      customerID: 1,
+      first: "FirstName",
+      last: "LastName",
+      address: "123 LaLa Lane",
+      zip: "La La, LA, 54321",
+      email: "email@email.com",
+      phone: "5551115555",
+      history: "notes and notes"
+    },
+    {
+      customerID: 2,
+      first: "AnotherFirst",
+      last: "AnotherLast",
+      address: "333 Boop Boop Drive",
+      zip: "Beepbop, BO, 54321",
+      email: "anotheremail@email.com",
+      phone: "5551114321",
+      history: "notes and notes and notes and notes"
+    }
 
+
+  ]
 
   constructor(private http: HttpClient) {}
 
